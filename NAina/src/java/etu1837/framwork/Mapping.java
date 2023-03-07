e4e287bc4241886b4e1a9b449c442920c032febb
fanyah <119622786+fanyahMi@users.noreply.github.com>
2023-03-07 14:16:34 +0300

    sprint2

---
+++ NAina/src/java/etu1837/framwork/Mapping.java
@@ -0,0 +1,27 @@
+package etu1837.framwork;
+
+/**
+ * Mapping
+ */
+public class Mapping {
+
+    String classe;
+    String methode;
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+
+    public String getMethode() {
+        return methode;
+    }
+
+    public void setMethode(String methode) {
+        this.methode = methode;
+    }
+
+}
